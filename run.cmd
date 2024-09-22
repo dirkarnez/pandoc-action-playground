574975ea41102e7b62bb5d6ccabb761783e10384
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-23 01:09:11 +0800

    - add run.cmd

---
+++ run.cmd
@@ -0,0 +1,15 @@
+set PATH=^
+%USERPROFILE%\Downloads\PortableGit\bin;
+
+:end
+cls &&^
+cd %cd% &&^
+C:\Windows\System32\timeout.exe /t 5 &&^
+echo committing &&^
+git config --global credential.helper "" &&^
+git add * &&^
+git commit -m "- upload files" &&^
+git push -u origin main
+goto end
+
+pause
