a1d9dbc799c46304ce30c9054e7000bd7c04e487
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-23 01:00:55 +0800

    - update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+apt-get install -y fonts-dejavu
+
 cd /data && \
 /usr/local/bin/pandoc sample.md \
 -V linkcolor:blue \
 -V geometry:a4paper \
 -V geometry:margin=2cm \
+-V fontsize=12pt \
+-V mainfont="DejaVu Serif" \
+-V monofont="DejaVu Sans Mono" \
 --pdf-engine=xelatex \
 -o sample.pdf
